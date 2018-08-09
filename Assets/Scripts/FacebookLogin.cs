16fe1283a9483cbbf2f5313c4c914a42687a042a
Tarcísio Vitor de Souza <tarcisiovity@hotmail.com>
2018-08-08 22:49:39 -0300

    FacebookLogin

---
+++ Assets/Scripts/FacebookLogin.cs
@@ -0,0 +1,32 @@
+﻿using Facebook.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacebookLogin : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        if (FB.IsInitialized == false)
+        {
+            FB.Init(() =>
+            {
+                if (FB.IsInitialized)
+                    FB.ActivateApp();
+                else
+                    Debug.LogError("Erro");
+            });
+        }
+        else
+            FB.ActivateApp();
+    }
+
+    public void ExecuteLogin()
+    {
+        var permission = new List<string>() { "public_profile", "email" };
+        FB.LogInWithReadPermissions(permission, (fbResult) => {
+            Debug.Log(fbResult.ResultDictionary.ToJson());
+        });
+    }
+}
