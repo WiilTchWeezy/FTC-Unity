f45096481ad0ebbe32e771eafae965c9b3ae8344
Tarcísio Vitor de Souza <tarcisiovity@hotmail.com>
2018-08-31 00:41:23 -0300

    Records

--- Assets/Scripts/FacebookLogin.cs
+++ Assets/Scripts/FacebookLogin.cs
@@ -1,7 +1,10 @@
 ﻿using Facebook.Unity;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
 
 public class FacebookLogin : MonoBehaviour
 {
@@ -25,8 +28,71 @@ public class FacebookLogin : MonoBehaviour
     public void ExecuteLogin()
     {
         var permission = new List<string>() { "public_profile", "email" };
-        FB.LogInWithReadPermissions(permission, (fbResult) => {
+        FB.LogInWithReadPermissions(permission, (fbResult) =>
+        {
             Debug.Log(fbResult.ResultDictionary.ToJson());
+            FB.API("/me?fields=id,name,email", HttpMethod.GET, GetFacebookInfo, new Dictionary<string, string>() { });
         });
     }
+
+    public void GetFacebookInfo(IResult result)
+    {
+        if (result.Error == null)
+        {
+            Debug.Log(result.ResultDictionary["id"].ToString());
+            Debug.Log(result.ResultDictionary["name"].ToString());
+            Debug.Log(result.ResultDictionary["email"].ToString());
+            PostBestScore(result.ResultDictionary["email"].ToString(), result.ResultDictionary["id"].ToString(), result.ResultDictionary["name"].ToString());
+        }
+        else
+        {
+            Debug.Log(result.Error);
+        }
+    }
+
+    private void PostBestScore(string email, string facebookId, string name)
+    {
+        var request = new GameApiRequest { Email = email, Name = name, Password = facebookId };
+        var jsonStrinRequest = JsonUtility.ToJson(request);
+        byte[] bytes =  System.Text.Encoding.UTF8.GetBytes(jsonStrinRequest);
+        int bestScore = 0;
+        bestScore = LoadGameBestScore();
+        Dictionary<string, string> headers = new Dictionary<string, string>();
+        headers.Add("Content-Type", "application/json");
+        WWW www = new WWW("http://gamesapi.somee.com/api/game/user?bestScore=" + bestScore, bytes, headers);
+        StartCoroutine(WaitForRequest(www));
+    }
+
+    private int LoadGameBestScore()
+    {
+        if (PlayerPrefs.HasKey("bestScore"))
+            return PlayerPrefs.GetInt("bestScore", 0);
+        else
+            return 0;
+    }
+
+    IEnumerator WaitForRequest(WWW www)
+    {
+        yield return www;
+        if (www.error == null)
+        {
+            //Print server response
+            Debug.Log(www.text);
+            SceneManager.LoadScene(2);
+        }
+        else
+        {
+            //Something goes wrong, print the error response
+            Debug.Log(www.error);
+        }
+    }
+
+}
+
+[Serializable]
+public class GameApiRequest
+{
+    public string Email;
+    public string Name;
+    public string Password;
 }
---
+++ Assets/Scripts/Records.cs
@@ -0,0 +1,104 @@
+﻿using Facebook.Unity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Records : MonoBehaviour
+{
+    public List<Text> RecordNames;
+    public List<Text> RecordPoints;
+    public RawImage img;
+
+    private string a = "";
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        foreach (var item in RecordNames)
+            item.text = "";
+        foreach (var item in RecordPoints)
+            item.text = "";
+
+        WWW www = new WWW("http://gamesapi.somee.com/api/game/score");
+        yield return www;
+        if (www.error == null)
+        {
+            Processjson(www.text);
+            FB.Init();
+            FB.API("/me/picture?redirect=false", HttpMethod.GET, ProfilePhotoCallback);
+        }
+        else
+        {
+            Debug.Log("ERROR: " + www.error);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void Processjson(string jsonString)
+    {
+        Score[] scores;
+        scores = JsonHelper.getJsonArray<Score>(jsonString);
+        if (scores != null)
+        {
+            for (int i = 0; i < scores.Length; i++)
+            {
+                Debug.Log(scores[i].Name);
+                Debug.Log(scores[i].ScorePoint);
+                Debug.Log(scores[i].Password);
+                RecordNames[i].text = scores[i].Name;
+                RecordPoints[i].text = scores[i].ScorePoint.ToString();
+                a = scores[i].Password;
+            }
+        }
+    }
+
+
+    private void ProfilePhotoCallback(IGraphResult result)
+    {
+        if (String.IsNullOrEmpty(result.Error) && !result.Cancelled)
+        {
+            IDictionary data = result.ResultDictionary["data"] as IDictionary;
+            string photoURL = data["url"] as String;
+
+            StartCoroutine(fetchProfilePic(photoURL));
+        }
+    }
+
+    private IEnumerator fetchProfilePic(string url)
+    {
+        WWW www = new WWW(url);
+        yield return www;
+        img.texture = www.texture;
+    }
+}
+
+[Serializable]
+public class Score
+{
+    public int ScorePoint;
+    public string Name;
+    public string Password;
+}
+
+public class JsonHelper
+{
+    public static T[] getJsonArray<T>(string json)
+    {
+        string newJson = "{ \"array\": " + json + "}";
+        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(newJson);
+        return wrapper.array;
+    }
+
+    [System.Serializable]
+    private class Wrapper<T>
+    {
+        public T[] array;
+    }
+}
+
